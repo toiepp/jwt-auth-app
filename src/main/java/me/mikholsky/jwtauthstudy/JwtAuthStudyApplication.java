b85e8d0c2698eff8405a8b469285397e717a736a
Mikholskiy Ivan <ivanmikhmain@gmail.com>
2023-11-07 15:43:50 +0300

    initializing project

---
+++ src/main/java/me/mikholsky/jwtauthstudy/JwtAuthStudyApplication.java
@@ -0,0 +1,13 @@
+package me.mikholsky.jwtauthstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuthStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/mikholsky/jwtauthstudy/JwtAuthStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mikholsky.jwtauthstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
