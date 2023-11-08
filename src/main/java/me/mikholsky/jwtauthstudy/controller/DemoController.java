b4232c9d52b98da9ceeac0c35e620d9c0ff28821
Mikholskiy Ivan <ivanmikhmain@gmail.com>
2023-11-08 20:43:54 +0300

    switch to docker compose launch

--- src/main/java/me/mikholsky/jwtauthstudy/controller/DemoController.java
+++ src/main/java/me/mikholsky/jwtauthstudy/controller/DemoController.java
@@ -1,6 +1,11 @@
 package me.mikholsky.jwtauthstudy.controller;
 
+import me.mikholsky.jwtauthstudy.controller.body.AuthenticationResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @GetMapping
     public ResponseEntity<String> sayHello() {
+        SecurityContext context = SecurityContextHolder.getContext();
+
+        Authentication authentication = context.getAuthentication();
+
+        System.out.println(authentication);
+
+        Object principal = authentication.getPrincipal();
+
+        System.out.println(principal);
+
         return ResponseEntity.ok("I'm secured!");
     }
 }
